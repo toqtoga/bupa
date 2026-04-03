dc52ec27284329b6c3060a74c0862a2854e05681
Toqtoga <toqtoga@proton.me>
2026-04-03 18:03:40 +1100

    update

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    setupFiles: ["./src/test/setup.ts"],
+    setupFiles: ["./src/tests/setup.ts"],
     typecheck: { enabled: true },
     watch: false,
     exclude: [
