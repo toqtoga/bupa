e12026f33ab6c6537a7a4598279c329bbb994802
Toqtoga <toqtoga@proton.me>
2026-04-03 18:11:35 +1100

    bit more coverage

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Bupa app
 
 * Initialised with `npm create vite@latest bupa -- --template react-ts`
-
+* Libraries used: `react-query`, `vitest`
 
 How to run
 * `npm install`
@@ -9,4 +9,12 @@ How to run
 
 To run tests
 * `npm run test`
+  * Running all except for `*spec.ts`
 * `npm run test:browser`
+  * Running all `*spec.tsx`
+
+Tests contain:
+* Testing the hook
+* Testing the parsing function used by the hook select function
+* Testing the resultant components (screenshot tests)
+* Testing interactivity from the main component (`OwnerAndBooks`)
--- src/hooks/useFetchBooks.test.tsx
+++ src/hooks/useFetchBooks.test.tsx
@@ -5,6 +5,7 @@ import { parseBookOwners, useFetchBookOwners } from "./useFetchBooks";
 
 import { setupServer } from "msw/node";
 import { handlers } from "../tests/mockHandlers";
+import bookOwnerData from "../tests/fixtures/owner";
 
 const server = setupServer(...handlers);
 
@@ -59,6 +60,55 @@ describe("useFetchBooks", async () => {
 
     await waitFor(() => expect(result.current.isSuccess).toBe(true));
     expect(result.current.data).toEqual([
+      {
+        books: [
+          "A Midsummer Night's Dream",
+          "React: The Ultimate Guide",
+          "Gulliver's Travels",
+          "Great Expectations",
+        ],
+        title: "Adults",
+      },
+      {
+        books: ["Great Expectations", "Little Red Riding Hood"],
+        title: "Children",
+      },
+    ]);
+  });
+});
+
+describe("parseBookOwners", () => {
+  it("parses book owners correctly in all books mode", () => {
+    const data = bookOwnerData;
+    const parser = parseBookOwners()(data);
+    expect(parser).toEqual([
+      {
+        books: [
+          "A Midsummer Night's Dream",
+          "Wuthering Heights",
+          "React: The Ultimate Guide",
+          "Gulliver's Travels",
+          "Jane Eyre",
+          "Great Expectations",
+        ],
+        title: "Adults",
+      },
+      {
+        books: [
+          "A Midsummer Night's Dream",
+          "Great Expectations",
+          "Little Red Riding Hood",
+          "The Hobbit",
+        ],
+        title: "Children",
+      },
+    ]);
+  });
+
+  it("parses book owners correctly in hardcover mode", () => {
+    const data = bookOwnerData;
+    const parser = parseBookOwners("Hardcover")(data);
+    expect(parser).toEqual([
       {
         books: [
           "A Midsummer Night's Dream",
